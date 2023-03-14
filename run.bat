b420f0316d2646e70316417ecaa80fd1d8f4194d
RB <biasedclover@gmail.com>
2023-03-14 16:56:40 +0800

    2023/03/14 周二 - 16:56:40.53

--- run.bat
+++ run.bat
@@ -4,4 +4,17 @@ REM Add custom JVM arguments to the user_jvm_args.txt
 REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
 REM  pass them to this script directly
 java  @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-43.1.1/win_args.txt %*
+setlocal
+
+
+echo Adding files to staging area...
+git add .
+set commit_message = "%date% - %time%"
+echo Committing changes with message "%commit_message%"...
+git commit -m "%commit_message%"
+
+echo Pushing changes to remote repository...
+git push origin main
+
+echo Done.
 pause
